97be57caa164e2ef4dde0306e80555ed1a278c65
Your Name <your.email@example.com>
2025-12-10 03:58:40 +0600

    authentication form added

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '../Firebase/firebase.config';
+
+const AuthProvider = ({children}) => {
+    const [user,setUser]=useState(null);
+    const [loading,setLoading]=useState(true);
+
+    const createUser=(email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+    const loginUser=(email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    const logOut=()=>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unsubscribe=auth.onAuthStateChanged(currentUser=>{
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return ()=>{
+            unsubscribe();
+        }
+    },[]);
+    const authInfo ={
+        createUser,
+        user,
+        loading,
+        loginUser,
+        logOut
+    };
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router';
+import Navbar from '../../components/Navbar';
 
 const Register = () => {
     return (
         <div>
-            register
+            <Navbar></Navbar>
+            <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Register Now!</h1>
+            <p className="py-6">
+                Let's build a safer community together. Join us today and start reporting issues in your area to help make a difference!
+            </p>
+            </div>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+                <form>
+                    <fieldset className="fieldset">
+                    <label className="label">Name</label>
+                    <input type="email" className="input" name='name' placeholder="Write your name" />
+                    <label className="label">Photo</label>
+                    <input type="email" className="input" name='photo' placeholder="give your latest photo" />
+                    <label className="label">Email</label>
+                    <input type="email" className="input" name='email' placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" className="input" name='password' placeholder="Password" />
+                    <button className="btn btn-neutral mt-4">Register</button>
+                    <div>Already have an account? <Link className='text-blue-500' to='/login'>Login</Link></div>
+                    <button type='button' className="btn bg-white text-black border-[#e5e5e5]">
+                    <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                    SignUp with Google
+                    </button>
+                    </fieldset>
+                </form>
+            </div>
+            </div>
+        </div>
+        </div>
         </div>
     );
 };
